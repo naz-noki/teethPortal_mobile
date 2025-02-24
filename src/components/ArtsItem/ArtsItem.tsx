6fe091ff67ec40983cc897ea11baa0a1ef704338
naz-noki <nazargolcheko@gmail.com>
2025-02-25 00:31:49 +0300

    feat: Add navigation for slides in ArtsItem component.

--- src/components/ArtsItem/ArtsItem.tsx
+++ src/components/ArtsItem/ArtsItem.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useState, useRef } from "react";
 import styles from "@/components/ArtsItem/ArtsItem.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { I_Art } from "@/constants/arts";
@@ -14,18 +14,71 @@ const ArtsItem: FC<I_ArtsItemProps> = ({
   className,
   style,
 }) => {
+  const slidesCounter = item.fileIds.length;
+  const [activeSlideIdx, setActiveSlideIdx] = useState(0);
+  const swiperRef = useRef<any>(null);
+
+  const handleNextSlide = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
+  const handlePrevSlide = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
   return (
     <div
       className={`${styles.wrapper} ${className && className}`}
       style={style}
     >
-      <Swiper spaceBetween={28} className={styles.wrapper__slider}>
-        {item.fileIds.map((el, idx) => (
-          <SwiperSlide key={idx} className={styles.wrapper__slider_slide}>
-            <img src={el} className={styles.wrapper__slider_img} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className={styles.wrapper__slider_block}>
+        <img
+          src="/teethPortal_mobile/icons/arrowLeft.svg"
+          alt="Arrow down icon"
+          className={`
+            ${styles.wrapper__slider_icon}
+            ${
+              slidesCounter > 1 &&
+              activeSlideIdx !== 0 &&
+              styles.wrapper__slider_icon_active
+            }
+          `}
+          onClick={() => handlePrevSlide()}
+        />
+        <Swiper
+          spaceBetween={28}
+          className={styles.wrapper__slider}
+          onSlideChange={(swiper) => setActiveSlideIdx(swiper.activeIndex)}
+          ref={swiperRef}
+        >
+          {item.fileIds.map((el, idx) => (
+            <SwiperSlide key={idx} className={styles.wrapper__slider_slide}>
+              <img
+                src={el}
+                className={styles.wrapper__slider_img}
+                alt={`Slide ${idx}`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <img
+          src="/teethPortal_mobile/icons/arrowRight.svg"
+          alt="Arrow down icon"
+          className={`
+            ${styles.wrapper__slider_icon}
+            ${
+              slidesCounter > 1 &&
+              activeSlideIdx !== slidesCounter - 1 &&
+              styles.wrapper__slider_icon_active
+            }
+          `}
+          onClick={() => handleNextSlide()}
+        />
+      </div>
       <div className={styles.wrapper__info}>
         <div className={styles.wrapper__info_block}>
           <p className={styles.wrapper__info_text}>{item.title}</p>
