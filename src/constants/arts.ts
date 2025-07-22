66d4d8c00560d81235ad34736684f7ab370126ce
NoK1 <nazargolcheko@gmail.com>
2025-07-22 09:59:21 +0300

    fix: Updated arts.

--- src/constants/arts.ts
+++ src/constants/arts.ts
@@ -8,7 +8,7 @@ export enum ART_TYPES {
 export interface I_Art {
   title: string;
   price?: string;
-  year: number;
+  year: string;
   material?: string;
   base?: string;
   size?: string;
@@ -41,31 +41,15 @@ export const ArtsTabBar = [
 const paintings: I_Art[] = [
   {
     title: "летний день",
-    year: 2025,
+    year: "2025",
     size: "15х20",
     material: "масло",
     base: "холст на картоне",
     fileIds: ["/teethPortal_mobile/images/summer_day.jpg"],
   },   
   {
-    title: "18: хризантема",
-    year: 2025,
-    size: "10х10",
-    material: "акрил, масло",
-    base: "холст на картоне",
-    fileIds: ["/teethPortal_mobile/images/18_хризантема.png"],
-  },     
-  {
-    title: "ласточка",
-    year: 2025,
-    size: "10х10",
-    material: "акрил, масло",
-    base: "холст на картоне",
-    fileIds: ["/teethPortal_mobile/images/ласточка.png"],
-  },
-  {
     title: "волна",
-    year: 2025,
+    year: "2025",
     size: "50х40",
     material: "масло, акрил",
     base: "холст на картоне",
@@ -73,7 +57,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "приворот",
-    year: 2025,
+    year: "2025",
     size: "10x10",
     material: "масло",
     base: "холст на картоне",
@@ -81,7 +65,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "под деревом",
-    year: 2025,
+    year: "2025",
     size: "20x30",
     material: "масло",
     base: "холст на картоне",
@@ -89,7 +73,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "уязвимое положение",
-    year: 2025,
+    year: "2025",
     size: "15x20",
     material: "масло",
     base: "холст на картоне",
@@ -100,7 +84,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "проклятье голубых глаз",
-    year: 2025,
+    year: "2025",
     size: "50x50",
     material: "акрил, скульптурная паста, синие розы",
     base: "холст на подрамнике",
@@ -110,16 +94,20 @@ const paintings: I_Art[] = [
     ],
   },
   {
-    title: "18: роза",
-    year: 2024,
+    title: "18: роза / хризантема / ласточка",
+    year: "2024-2025",
     size: "10x10",
-    material: "масло",
+    material: "акрил, масло",
     base: "холст на картоне",
-    fileIds: ["/teethPortal_mobile/images/18 роза 1.png"],
+    fileIds: [
+      "/teethPortal_mobile/images/18 роза 1.png",
+      "/teethPortal_mobile/images/18_хризантема.png",
+      "/teethPortal_mobile/images/ласточка.png",
+    ],
   },
   {
     title: "энергия",
-    year: 2024,
+    year: "2024",
     size: "20x15 (25х20 с багетом)",
     material: "акрил",
     base: "холст на картоне",
@@ -130,7 +118,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "красивое лицо",
-    year: 2024,
+    year: "2024",
     size: "25x15",
     material: "акрил",
     base: "холст на картоне",
@@ -138,7 +126,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "птенец",
-    year: 2024,
+    year: "2024",
     size: "50x40",
     material: "акрил",
     base: "холст на подрамнике",
@@ -146,7 +134,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "пепельница",
-    year: 2024,
+    year: "2024",
     size: "20x20",
     material: "акрил, облепиховый чай, пепел",
     base: "холст на подрамнике",
@@ -157,7 +145,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "привязанность",
-    year: 2023,
+    year: "2023",
     size: "25x25",
     material: "акрил",
     base: "холст на картоне",
@@ -165,7 +153,7 @@ const paintings: I_Art[] = [
   },
   {
     title: "малиновый рассвет",
-    year: 2022,
+    year: "2022",
     size: "50x40",
     material: "акрил, пастель",
     base: "холст на картоне",
@@ -179,47 +167,47 @@ const paintings: I_Art[] = [
 const drawings: I_Art[] = [
   {
     title: "нервные клетки",
-    year: 2025,
+    year: "2025",
     fileIds: ["/teethPortal_mobile/images/nerveCells.png"],
   },
   {
     title: "стоящий под деревом",
-    year: 2025,
+    year: "2025",
     fileIds: ["/teethPortal_mobile/images/standingUnderTree.png"],
   },
   {
     title: "сухая тушь",
-    year: 2025,
+    year: "2025",
     fileIds: ["/teethPortal_mobile/images/сухая тушь.png"],
   },
   {
     title: "ежик",
-    year: 2025,
+    year: "2025",
     fileIds: ["/teethPortal_mobile/images/ежик.png"],
   },
   {
     title: "зеркальная",
-    year: 2025,
+    year: "2025",
     fileIds: ["/teethPortal_mobile/images/зеркальная.png"],
   },
   {
     title: "натура номер один",
-    year: 2025,
+    year: "2025",
     fileIds: ["/teethPortal_mobile/images/натура номер один.png"],
   },
   {
     title: "танцы в ресторане",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/танцы в ресторане.png"],
   },
   {
     title: "красивое лицо. эскиз",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/красивое лицо. эскиз.png"],
   },
   {
     title: "секрет лица",
-    year: 2024,
+    year: "2024",
     fileIds: [
       "/teethPortal_mobile/images/секрет лица_1.png",
       "/teethPortal_mobile/images/секрет лица_2.png",
@@ -227,22 +215,22 @@ const drawings: I_Art[] = [
   },
   {
     title: "перед выдохом",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/перед выдохом.png"],
   },
   {
     title: "я выросла",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/я выросла.png"],
   },
   {
     title: "красный мячик",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/красный мячик.png"],
   },
   {
     title: "после встречи",
-    year: 2024,
+    year: "2024",
     fileIds: [
       "/teethPortal_mobile/images/после встречи_1.png",
       "/teethPortal_mobile/images/после встречи_2.png",
@@ -250,7 +238,7 @@ const drawings: I_Art[] = [
   },
   {
     title: "перед встречей",
-    year: 2024,
+    year: "2024",
     fileIds: [
       "/teethPortal_mobile/images/перед встречей_1.png",
       "/teethPortal_mobile/images/перед встречей_2.png",
@@ -259,177 +247,177 @@ const drawings: I_Art[] = [
   },
   {
     title: "ангел, я скучаю",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/ангел, я скучаю.png"],
   },
   {
     title: "родительская любовь",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/родительская любовь.png"],
   },
   {
     title: "ангельская любовь",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/ангельская любовь.png"],
   },
   {
     title: "метаморфоза",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/метаморфоза.png"],
   },
   {
     title: "колодец",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/колодец.png"],
   },
   {
     title: "холодная зима",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/холодная зима.png"],
   },
   {
     title: "теплая зима",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/теплая зима.png"],
   },
   {
     title: "чайки",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/чайки.png"],
   },
   {
     title: "я+йа=я",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/я+йа=я.png"],
   },
   {
     title: "ресницы",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/ресницы.png"],
   },
   {
     title: "добро пожаловать",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/добро пожаловать.png"],
   },
   {
     title: "роберт во тьме",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/роберт во тьме.png"],
   },
   {
     title: "небеса в автобусе",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/небеса в автобусе.png"],
   },
   {
     title: "отпусти",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/отпусти.png"],
   },
   {
     title: "спасибо, что не испугался",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/спасибо, что не испугался.png"],
   },
   {
     title: "кусь",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/кусь.png"],
   },
   {
     title: "вы кое-что обронили",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/вы кое-что обронили.png"],
   },
   {
     title: "рождение",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/рождение.png"],
   },
   {
     title: "замочек пальцами и ребрами",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/замочек пальцами и ребрами.png"],
   },
   {
     title: "прошлое настоящее будущее",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/IMG_0589.PNG"],
   },
   {
     title: "обсидиан",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/обсидиан.png"],
   },
   {
     title: "пустые глазницы",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/пустые глазницы.png"],
   },
   {
     title: "паутина",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/паутина.png"],
   },
   {
     title: "молчание",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/молчание.png"],
   },
   {
     title: "целое",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/целое.png"],
   },
   {
     title: "живой стул",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/живой стул.png"],
   },
   {
     title: "дракон из сна",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/дракон из сна.png"],
   },
   {
     title: "лица луни",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/лица луни.png"],
   },
   {
     title: "котэ",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/котэ.png"],
   },
   {
     title: "выход всегда есть",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/выход всегда есть.png"],
   },
   {
     title: "упадок с элементами ясности",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/упадок с элементами ясности.png"],
   },
   {
     title: "йа – опыт или интерпретация",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/йа опыт или.png"],
   },
   {
     title: "лунный свет",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/лунный свет.png"],
   },
   {
     title: "приглядывает",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/приглядывает.png"],
   },
   {
     title: "желаю не терять",
-    year: 2022,
+    year: "2022",
     size: "А5",
     base: "открытка на черной бумаге",
     fileIds: [
@@ -440,22 +428,22 @@ const drawings: I_Art[] = [
   },
   {
     title: "переломный момент",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/переломный момент.png"],
   },
   {
     title: "проснись",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/проснись.png"],
   },
   {
     title: "женские руки",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/женские руки.png"],
   },
   {
     title: "ложь",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/ложь.png"],
   },
 ];
@@ -463,22 +451,22 @@ const drawings: I_Art[] = [
 const digital: I_Art[] = [
   {
     title: "портал",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/teethportal.png"],
   },
   {
     title: "98",
-    year: 2024,
+    year: "2024",
     fileIds: ["/teethPortal_mobile/images/98.png"],
   },
   {
     title: "whereismymind",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/whereismymind.png"],
   },
   {
     title: "выпуск",
-    year: 2023,
+    year: "2023",
     fileIds: [
       "/teethPortal_mobile/images/выпуск_1.png",
       "/teethPortal_mobile/images/выпуск_2.png",
@@ -486,7 +474,7 @@ const digital: I_Art[] = [
   },
   {
     title: "дерево в поле",
-    year: 2023,
+    year: "2023",
     fileIds: [
       "/teethPortal_mobile/images/дерево в поле_1.png",
       "/teethPortal_mobile/images/дерево в поле_2.png",
@@ -494,7 +482,7 @@ const digital: I_Art[] = [
   },
   {
     title: "ангел меня не покинет",
-    year: 2023,
+    year: "2023",
     fileIds: [
       "/teethPortal_mobile/images/ангел меня не покинет_1.png",
       "/teethPortal_mobile/images/ангел меня не покинет_2.png",
@@ -502,34 +490,34 @@ const digital: I_Art[] = [
   },
   {
     title: "ино",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/ино.png"],
   },
   {
     title: "мысленных образов не обнаружено",
-    year: 2023,
+    year: "2023",
     fileIds: [
       "/teethPortal_mobile/images/мысленных образов поблизости не обнаружено.png",
     ],
   },
   {
     title: "лицо луни",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/лицо луни.png"],
   },
   {
     title: "соединение атомов",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/соединение атомов.gif"],
   },
   {
     title: "тише, она уснула",
-    year: 2023,
+    year: "2023",
     fileIds: ["/teethPortal_mobile/images/тише, она уснула.png"],
   },
   {
     title: "просвет",
-    year: 2022,
+    year: "2022",
     fileIds: ["/teethPortal_mobile/images/просвет.png"],
   },
 ];
