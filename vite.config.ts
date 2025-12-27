b22cef28eeba6e4c19c0be68a69b92601db6b9ee
NoK1 <nazargolcheko@gmail.com>
2025-12-27 23:37:11 +0300

    fix: Fixed build.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,22 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/teethPortal_mobile",
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+
+  // Отключить ненужные функции в продакшене
+  appType: "spa",
+  optimizeDeps: {
+    include: ["react", "react-dom", "react-router-dom"],
+    exclude: ["lottie-web"], // Исключить тяжелые библиотеки из предварительной оптимизации
+    esbuildOptions: {
+      target: "es2020",
     },
   },
 });
